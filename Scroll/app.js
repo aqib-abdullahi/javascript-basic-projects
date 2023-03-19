3f6d364058728956035374383812a04a09490baf
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-03-20 00:34:33 +0100

    added the back to top button functionality and fixed navbar based on pageOffset

--- Scroll/app.js
+++ Scroll/app.js
@@ -1,7 +1,30 @@
 const drop = document.querySelector(".ul-link")
 const dropButton = document.querySelector('.menu-button')
+const toTop = document.querySelector(".to-top")
+const plainNav = document.querySelector(".second-nav")
+const fixedNav = document.querySelector(".first-nav")
 
 dropButton.addEventListener("click", function() {
     drop.classList.toggle('drop')
     console.log('clicked');
+})
+
+toTop.addEventListener("click", function() {
+    let h = document.body.getBoundingClientRect().height
+    window.scrollTo({
+        left: 0,
+        top: 0
+    })
+})
+
+window.addEventListener('scroll', function() {
+    let height = window.pageYOffset
+    let secondNavHeight = plainNav.getBoundingClientRect().height
+    if (height >= secondNavHeight) {
+        fixedNav.classList.add('fnav')
+    } else if (height == 0) {
+        fixedNav.classList.remove('fnav')
+    }
+
+
 })
