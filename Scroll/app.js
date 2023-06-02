ffd5b4b790b0c85dd5fe8335e5d0ef189f1f874a
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-06-02 22:52:06 +0100

    Added dynamic content loading for navbar in mobile view

--- Scroll/app.js
+++ Scroll/app.js
@@ -3,10 +3,21 @@ const dropButton = document.querySelector('.menu-button')
 const toTop = document.querySelector(".to-top")
 const plainNav = document.querySelector(".second-nav")
 const fixedNav = document.querySelector(".first-nav")
+const CopyDate = document.querySelector(".date")
+const linkH = document.querySelector('.ul-link')
+const listH = document.querySelector('.ul-list')
+
+newDate = new Date().getFullYear()
+CopyDate.textContent = newDate
 
 dropButton.addEventListener("click", function() {
     drop.classList.toggle('drop')
-    console.log('clicked');
+    if (drop.getBoundingClientRect().height == 0) {
+        let listHH = listH.getBoundingClientRect().height
+        linkH.style.height = `${listHH + 30}px`
+    } else {
+        linkH.style.height = 0
+    }
 })
 
 toTop.addEventListener("click", function() {
@@ -18,14 +29,20 @@ toTop.addEventListener("click", function() {
     })
 })
 
+
 window.addEventListener('scroll', function() {
+
     let height = window.pageYOffset
+    let fixedNavHeight = fixedNav.getBoundingClientRect().height
+    if (height >= fixedNavHeight * 2) {
+        toTop.classList.add('showBtn')
+    } else {
+        toTop.classList.remove('showBtn')
+    }
     let secondNavHeight = plainNav.getBoundingClientRect().height
     if (height >= secondNavHeight) {
         fixedNav.classList.add('fnav')
     } else if (height == 0) {
         fixedNav.classList.remove('fnav')
     }
-
-
 })
